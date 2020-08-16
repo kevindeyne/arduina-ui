f7ed6aeccd7b070aff23f3f2bfa5e6a78edc05b7
Kevin Deyne <kevindeyne@gmail.com>
2020-08-16 13:11:33 -0700

    Basic router setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { ProjectsComponent } from './projects/projects.component';
+import { TeamComponent } from './team/team.component';
+import { TestcasesComponent } from './testcases/testcases.component';
+
+const appRoutes : Routes = [
+  { path: '', component: TeamComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'testcases', component: TestcasesComponent }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    TeamComponent,
+    TestcasesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
