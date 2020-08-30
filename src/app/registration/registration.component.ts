e1fadbe4b4fd8d24e4da56a560ab058ab753dbcc
Kevin Deyne <kevindeyne@gmail.com>
2020-08-29 21:30:00 -0700

    cleanup registration component

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
+  username = '';
+  password = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  register() {}
 
 }
